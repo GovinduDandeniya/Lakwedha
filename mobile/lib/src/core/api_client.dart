28d1347e17b2405960ed9a3cfd9d97a5b3ddb19a
Mubarak-15 <mubarakfasni@gmail.com>
2026-02-28 15:35:54 +0530

    secured api client to automatically inject jwt token on outgoing requests

---
+++ mobile/lib/src/core/api_client.dart
@@ -0,0 +1,49 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:logger/logger.dart';
+
+final loggerProvider = Provider((ref) => Logger());
+
+const _storage = FlutterSecureStorage();
+
+final dioProvider = Provider((ref) {
+  final dio = Dio(
+    BaseOptions(
+      // Using 10.0.2.2 for Android Emulator to access localhost
+      baseUrl: 'http://10.0.2.2:5000/api',
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    ),
+  );
+
+  final logger = ref.read(loggerProvider);
+
+  dio.interceptors.add(InterceptorsWrapper(
+    onRequest: (options, handler) async {
+      logger.i('REQUEST[${options.method}] => PATH: ${options.path}');
+
+      // Inject Authorization token if it exists
+      final token = await _storage.read(key: 'jwt_token');
+      if (token != null && token.isNotEmpty) {
+        options.headers['Authorization'] = 'Bearer $token';
+      }
+
+      return handler.next(options);
+    },
+    onResponse: (response, handler) {
+      logger.i('RESPONSE[${response.statusCode}] => DATA: ${response.data}');
+      return handler.next(response);
+    },
+    onError: (DioException e, handler) {
+      logger.e('ERROR[${e.response?.statusCode}] => MESSAGE: ${e.message}');
+      return handler.next(e);
+    },
+  ));
+
+  return dio;
+});
