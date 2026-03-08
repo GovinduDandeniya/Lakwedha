24af1689c2d59488e593a91c2aeb06d7a25f565b
SandaruPasan <wsandarupasan@gmail.com>
2026-03-08 21:59:36 +0530

    Update forgot_password_screen.dart

---
+++ mobile/lib/features/auth/screens/forgot_password_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ForgotPasswordScreen extends StatefulWidget {
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
