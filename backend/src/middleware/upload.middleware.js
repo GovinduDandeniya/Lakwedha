e24a96538bd5f1c9ce5d761d70ad0cb5b64d07cd
ilham faiz <ilhxmfxiz@gmail.com>
2026-03-08 19:40:23 +0530

    edit file

--- backend/src/middleware/upload.middleware.js
+++ backend/src/middleware/upload.middleware.js
@@ -8,7 +8,16 @@ if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
 
-
+// Set up Multer storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        cb(null, 'prescription-' + uniqueSuffix + path.extname(file.originalname));
+    }
+});
 
 // File type filter
 const fileFilter = (req, file, cb) => {
