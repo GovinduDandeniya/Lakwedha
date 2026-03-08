c5dc74bfb6cc1d02cf9f7f6ee59ccd159f902092
SandaruPasan <wsandarupasan@gmail.com>
2026-03-08 20:57:07 +0530

    Change to key

--- mobile/lib/core/services/storage_service.dart
+++ mobile/lib/core/services/storage_service.dart
@@ -1,7 +1,7 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class StorageService {
-  static const _token = 'auth_token';
-  static const _userName = 'user_name';
-  static const _isGuest = 'is_guest';
+  static const _tokenKey = 'auth_token';
+  static const _userNameKey = 'user_name';
+  static const _isGuestKey = 'is_guest';
 }
