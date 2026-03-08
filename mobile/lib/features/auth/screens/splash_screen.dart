5a51a77e5d3ab357a7294cc466bd651ebb9135a0
SandaruPasan <wsandarupasan@gmail.com>
2026-03-08 22:34:53 +0530

    Import splash screen packages

--- mobile/lib/features/auth/screens/splash_screen.dart
+++ mobile/lib/features/auth/screens/splash_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import '../../../core/constants/app_colors.dart';
+import '../../../core/services/storage_service.dart';
+
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
