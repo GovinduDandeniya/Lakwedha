7778921c3e5b233f218b92a29364d88ec502939d
SandaruPasan <wsandarupasan@gmail.com>
2026-03-08 19:39:05 +0530

    Update app_colors.dart Add Primary palette

--- mobile/lib/core/constants/app_colors.dart
+++ mobile/lib/core/constants/app_colors.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+
+  // Primary palette
+  static const Color primary = Color(0xFF6B0000);       
+  static const Color primaryDark = Color(0xFF4A0000);
+  static const Color accent = Color(0xFFB8860B);        
+  static const Color accentLight = Color(0xFFD4A017);   
+  static const Color secondaryGreen = Color(0xFF2D5A27);
 }
