27e89a9623961704795147d86478a839012aabbf
SandaruPasan <wsandarupasan@gmail.com>
2026-03-08 22:33:27 +0530

    Import sign up packages

--- mobile/lib/features/auth/screens/sign_up_screen.dart
+++ mobile/lib/features/auth/screens/sign_up_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import '../../../core/constants/app_colors.dart';
+import '../../../core/constants/app_strings.dart';
+import '../../../core/services/auth_service.dart';
+import '../widgets/custom_text_field.dart';
+import '../widgets/auth_button.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
