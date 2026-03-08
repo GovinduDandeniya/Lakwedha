88737bcc60ae1ebe36a9428c23c94b607f91cff2
sesathde <sesathdesilva2003@gmail.com>
2026-03-09 00:39:04 +0530

    Create notificationService.js

---
+++ backend/src/Notifications/notificationService.js
@@ -0,0 +1,16 @@
+const Notification = require('../models/Notification');
+const User = require('../models/user');
+const { sendSMS } = require('./smsGateway');
+const templates = require('./smsTemplates');
+/** 
+ @param {string|ObjectId} userId
+ @param {string} type                       //appointment,order,prescription,system
+ @param {string} title                     // Short notification title
+ @param {string} message                   //Full notification message
+ @param {string|ObjectId} [referenceId]   //Related document ID
+ @param {string} [referenceModel]         //Related model name
+ @param {string} [smsMessage]             //Override SMS text (defaults to message)
+ @returns {Promise<Notification>}
+*/
+
+
