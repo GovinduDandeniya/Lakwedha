c89bd7b3ff64c075b7ae1bc636073394db03a06d
SandaruPasan <wsandarupasan@gmail.com>
2026-03-08 21:01:07 +0530

    update storage_service save JWT Token

--- mobile/lib/core/services/storage_service.dart
+++ mobile/lib/core/services/storage_service.dart
@@ -4,4 +4,9 @@ class StorageService {
   static const _tokenKey = 'auth_token';
   static const _userNameKey = 'user_name';
   static const _isGuestKey = 'is_guest';
+
+  static Future<void> saveToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_tokenKey, token);
+    }
 }
