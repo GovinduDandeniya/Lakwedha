3ed988f4ae707f2c5b01faa10b3c94d3d6cef53a
SandaruPasan <wsandarupasan@gmail.com>
2026-03-08 22:00:53 +0530

    Update sign_in_screen.dart

---
+++ mobile/lib/features/auth/screens/sign_in_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
