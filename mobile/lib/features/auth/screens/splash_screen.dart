c1a316eac6c9b9e7bd549f5dfc525580ef47fb48
SandaruPasan <wsandarupasan@gmail.com>
2026-03-08 22:03:12 +0530

    Update splash_screen.dart

---
+++ mobile/lib/features/auth/screens/splash_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
