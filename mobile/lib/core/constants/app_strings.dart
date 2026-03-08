79e85ea7b3e4ec771a52b3f985d0bfcffab0036a
SandaruPasan <wsandarupasan@gmail.com>
2026-03-08 19:57:00 +0530

    Update app_Strings.dart Add sign in and Sign Up

--- mobile/lib/core/constants/app_strings.dart
+++ mobile/lib/core/constants/app_strings.dart
@@ -2,4 +2,30 @@ class AppStrings {
 
   // App
   static const String appName = 'Ravana 2.0';
+
+  // Sign In
+  static const String signIn = 'Sign In';
+  static const String welcomeBack = 'Welcome to Ravana 2.0';
+  static const String credentialLabel = 'Mobile Number | Email or Username';
+  static const String credentialHint = 'Enter your credentials';
+  static const String passwordLabel = 'Password';
+  static const String passwordHint = 'Enter your password';
+  static const String forgotPassword = 'Forgot password?';
+  static const String continueAsGuest = 'Continue as Guest';
+  static const String newUser = "I'm new user ! ";
+  static const String signUpLink = 'Sign Up';
+
+  // Sign Up
+  static const String signUp = 'Sign Up';
+  static const String createAccount = 'Create Your Account';
+  static const String fullNameLabel = 'Full Name';
+  static const String fullNameHint = 'Enter your full name';
+  static const String emailLabel = 'Email';
+  static const String emailHint = 'Enter your email';
+  static const String confirmPasswordLabel = 'Confirm Password';
+  static const String confirmPasswordHint = 'Re-enter your password';
+  static const String alreadyHaveAccount = 'Already have an account? ';
+  static const String signInLink = 'Sign In';
+
+  
 }
