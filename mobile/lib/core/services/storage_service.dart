7e5d0b1760627621c881dd55dce568913425bab4
SandaruPasan <wsandarupasan@gmail.com>
2026-03-08 20:56:17 +0530

    Create storage_service
    class storageService

---
+++ mobile/lib/core/services/storage_service.dart
@@ -0,0 +1,7 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class StorageService {
+  static const _token = 'auth_token';
+  static const _userName = 'user_name';
+  static const _isGuest = 'is_guest';
+}
