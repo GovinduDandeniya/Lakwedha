0fb3b3266f5d9ab0a3b626eb00ec164cfae4ca87
SandaruPasan <wsandarupasan@gmail.com>
2026-03-08 23:43:47 +0530

    Update splash_screen.dart

--- mobile/lib/features/auth/screens/splash_screen.dart
+++ mobile/lib/features/auth/screens/splash_screen.dart
@@ -10,3 +10,26 @@ class SplashScreen extends StatefulWidget {
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnim;
+  late Animation<double> _fadeAnim;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1400),
+    );
+    _scaleAnim = CurvedAnimation(parent: _controller, curve: Curves.easeOutBack);
+    _fadeAnim = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: _controller, curve: const Interval(0.3, 1.0)),
+    );
+    _controller.forward();
+    _navigateAfterSplash();
+  }
+}
+
