8d923947719fbe7d5e3c5e7c6313b95585e429bd
ilham faiz <ilhxmfxiz@gmail.com>
2026-03-13 20:50:04 +0530

    feat: add line 'message: err.message  Internal Server Er' to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -47,4 +47,5 @@ app.use((err, req, res, next) => {
 
     return res.status(err.status || 500).json({
         success: false,
+        message: err.message || 'Internal Server Error'
 module.exports = app;
