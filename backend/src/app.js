a3952667a5d9732db485819ce48380a13b8aa6ad
ilham faiz <ilhxmfxiz@gmail.com>
2026-03-13 20:50:04 +0530

    feat: add line '});' to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -48,4 +48,5 @@ app.use((err, req, res, next) => {
     return res.status(err.status || 500).json({
         success: false,
         message: err.message || 'Internal Server Error'
+    });
 module.exports = app;
