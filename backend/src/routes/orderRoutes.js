4c727c1ed76a4256c3b4c10431648160937238d0
Mubarak-15 <mubarakfasni@gmail.com>
2026-03-22 03:57:37 +0530

    updated order routes for payhere endpoints

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -9,17 +9,20 @@ const {
     getOrderById,
     initiatePayment,
     confirmPayment,
-    handleStripeWebhook
+    handlePayhereNotification
 } = require('../controllers/orderController');
+
+// PayHere server-to-server webhook (defined before /:id)
+router.post('/pay/notify', handlePayhereNotification);
 
 // GET all orders - Protected
 router.get('/', auth, getAllOrders);
 
-// GET single order - Protected
-router.get('/:id', auth, getOrderById);
-
 // Create order from prescription - Protected
 router.post('/from-prescription/:prescriptionId', auth, createOrderFromPrescription);
+
+// GET single order - Protected
+router.get('/:id', auth, getOrderById);
 
 // Update payment status (Pharmacist / Admin) - Protected
 router.put('/:id/payment', auth, updatePaymentStatus);
@@ -32,8 +35,5 @@ router.post('/:id/pay/initiate', auth, initiatePayment);
 
 // Confirm Payment - Protected
 router.post('/:id/pay/confirm', auth, confirmPayment);
-
-// Stripe server-to-server webhook
-router.post('/pay/notify', handleStripeWebhook);
 
 module.exports = router;
